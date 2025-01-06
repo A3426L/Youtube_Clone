a6137571bc0211b19f136cc97951b391a883bfaf
A3426L <mikan7738@gmail.com>
2025-01-06 21:35:08 +0900

    add: google認証

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GoogleLoginController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/auth/redirect', [GoogleLoginController::class, 'getGoogleAuth']);
+Route::get('/auth/callback', [GoogleLoginController::class, 'authGoogleCallback']);
