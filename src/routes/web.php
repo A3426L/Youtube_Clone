74b696c134b161627e5c987212d8f444ab48725d
A3426L <mikan7738@gmail.com>
2025-01-08 23:26:16 +0900

    #4-add: 動画アップロード機能を追加

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,11 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GoogleLoginController;
+use App\Http\Controllers\TestController;
 
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/test/upload',[TestController::class,'upload']);
 Route::get('/auth/redirect', [GoogleLoginController::class, 'getGoogleAuth']);
 Route::get('/auth/callback', [GoogleLoginController::class, 'authGoogleCallback']);
+
