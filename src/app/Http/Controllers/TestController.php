f3a1cad8273c78a651e176491e928e3c90d2bdae
A3426L <mikan7738@gmail.com>
2025-01-13 23:21:34 +0900

    #7add: 動画アップロード機能のバリデーションとルート修正

--- src/app/Http/Controllers/TestController.php
+++ src/app/Http/Controllers/TestController.php
@@ -11,12 +11,25 @@ class TestController extends Controller
     }
 
     function upload(Request $request){
+        try{
+            $request->validate([
+                'video' => 'required|mimes:mp4,avi,mov|max:10240'
+            ]);
+        }catch (validationException $e){
+            dd("a");
+            return redirect('/tests/home')->with('error',$e->errors());
+        }
 
-        $request->validate([
-            'video' => 'required|mimes:mp4,avi,mov|max:10240'
-        ]);
+        
 
-        $path = $request->file('video')->store('')
+        $path = $request->file('video')->store('videos','pubilc');
+
+        dd($path);
+
+        //dbへ保存
+
+        return view('welcome');
+
 
     }
 }
